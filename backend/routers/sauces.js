a163247da3ad7d608fbdc283da10327ba5b1e3a4
Feifei Zhang <zwz108@gmail.com>
2025-08-25 22:18:02 -0700

    Fixed syntax error of put and delete routes.

--- backend/routers/sauces.js
+++ backend/routers/sauces.js
@@ -8,8 +8,8 @@ const saucesCtrl = require("../controllers/sauces");
 router.post("/", auth, saucesCtrl.createSauce);
 router.get("/", auth, saucesCtrl.findAllSauces);
 router.get("/:id", auth, saucesCtrl.findOneSauce);
-router.put("/:id", auth, updateOneSauce);
-router.delete("/:id", auth, deleteOneSauce);
+router.put("/:id", auth, saucesCtrl.updateOneSauce);
+router.delete("/:id", auth, saucesCtrl.deleteOneSauce);
 router.post('/:id/like', auth, saucesCtrl.setLike);
 
 module.exports = router;
