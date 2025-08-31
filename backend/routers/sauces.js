2828377d7f68927a8ebe01d80b9580300402d2d6
Feifei Zhang <zwz108@gmail.com>
2025-08-30 23:44:33 -0700

    Added multer to PUT request.

--- backend/routers/sauces.js
+++ backend/routers/sauces.js
@@ -9,7 +9,7 @@ const saucesCtrl = require("../controllers/sauces");
 router.post("/", auth, multer, saucesCtrl.createSauce);
 router.get("/", auth, saucesCtrl.findAllSauces);
 router.get("/:id", auth, saucesCtrl.findOneSauce);
-router.put("/:id", auth, saucesCtrl.updateOneSauce);
+router.put("/:id", auth, multer, saucesCtrl.updateOneSauce);
 router.delete("/:id", auth, saucesCtrl.deleteOneSauce);
 router.post('/:id/like', auth, saucesCtrl.setLike);
 
