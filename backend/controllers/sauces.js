c95163e6ab842a12ced781c046d1822abb3baecd
Feifei Zhang <zwz108@gmail.com>
2025-09-03 22:09:48 -0700

    Modified setLike method so that a user cannot like and dislike a sauce at the same time by having a reset function.

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,5 +1,9 @@
 const Sauce = require("../models/sauce");
+const fs = require("fs");
 
+/**
+ * fjfjfk
+ */
 exports.createSauce = (req, res, next) => {
   const url = req.protocol + "://" + req.get("host");
   req.body.sauce = JSON.parse(req.body.sauce);
@@ -36,42 +40,32 @@ exports.findOneSauce = (req, res, next) => {
 };
 
 exports.updateOneSauce = (req, res, next) => {
-  let sauce = new Sauce({_id: req.params.id});
-  
+  let sauce = new Sauce({ _id: req.params.id });
+  let sauceBody;
+  let imageUrl;
+
   if (req.file) {
     const url = req.protocol + "://" + req.get("host");
-    req.body.sauce = JSON.parse(req.body.sauce);
-
-    sauce = {
-      _id: req.params.id,
-      userId: req.auth.userId,
-      name: req.body.sauce.name,
-      manufacturer: req.body.sauce.manufacturer,
-      description: req.body.sauce.description,
-      mainPepper: req.body.sauce.mainPepper,
-      imageUrl: url + "/images/" + req.file.filename,
-      heat: req.body.sauce.heat,
-      likes: 0,
-      dislikes: 0,
-      usersLiked: [],
-      usersDisliked: [],
-    };
+    sauceBody = JSON.parse(req.body.sauce);
+    imageUrl = url + "/images/" + req.file.filename;
   } else {
-    sauce = {
-      _id: req.params.id,
-      userId: req.auth.userId,
-      name: req.body.name,
-      manufacturer: req.body.manufacturer,
-      description: req.body.description,
-      mainPepper: req.body.mainPepper,
-      imageUrl: req.body.imageUrl,
-      heat: req.body.heat,
-      likes: req.body.likes,
-      dislikes: req.body.dislikes,
-      usersLiked: req.body.usersLiked,
-      usersDisliked: req.body.usersDisliked,
-    };
+    sauceBody = req.body;
+    imageUrl = sauceBody.imageUrl;
   }
+  sauce = {
+    _id: req.params.id,
+    userId: req.auth.userId,
+    name: sauceBody.name,
+    manufacturer: sauceBody.manufacturer,
+    description: sauceBody.description,
+    mainPepper: sauceBody.mainPepper,
+    imageUrl,
+    heat: sauceBody.heat,
+    likes: sauceBody.likes,
+    dislikes: sauceBody.dislikes,
+    usersLiked: sauceBody.usersLiked,
+    usersDisliked: sauceBody.usersDisliked,
+  };
 
   Sauce.updateOne({ _id: req.params.id }, sauce)
     .then(() =>
@@ -92,10 +86,56 @@ exports.deleteOneSauce = (req, res, next) => {
         error: new Error("Unauthorized request!"),
       });
     }
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: "Deleted!" }))
-      .catch((err) => res.status(400).json({ error: err }));
+
+    const filename = sauce.imageUrl.split("/images/")[1];
+
+    fs.unlink("images/" + filename, () => {
+      Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Deleted!" }))
+        .catch((err) => res.status(400).json({ error: err }));
+    });
   });
 };
 
-exports.setLike = (req, res, next) => {};
+exports.setLike = (req, res, next) => {
+  const userId = req.auth.userId;
+  const like = req.body.like;
+
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    if (!sauce) {
+      return res.status(404).json({ error: new Error("No such sauce!") });
+    }
+
+    const resetUserPreference = () => {
+      if (sauce.usersLiked.includes(userId)) {
+        sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId);
+        sauce.likes--;
+      }
+      if (sauce.usersDisliked.includes(userId)) {
+        sauce.usersDisliked = sauce.usersDisliked.filter((id) => id !== userId);
+        sauce.dislikes--;
+      }
+    };
+
+    if (like === 1) {
+      resetUserPreference();
+      sauce.usersLiked.push(userId);
+      sauce.likes++;
+    }
+
+    if (like === 0) {
+      resetUserPreference();
+    }
+
+    if (like === -1) {
+      resetUserPreference();
+      sauce.usersDisliked.push(userId);
+      sauce.dislikes++;
+    }
+
+    sauce
+      .save()
+      .then(() => res.status(200).json({ message: "Preference updated!" }))
+      .catch((err) => res.status(400).json({ error: err }));
+  });
+};
